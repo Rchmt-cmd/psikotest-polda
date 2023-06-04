db3cfeedfeb01f51200e43a66a01ea899722833e
Rchmt-cmd <rachmath.059@gmail.com>
2023-06-04 11:16:57 +0800

    update landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -516,50 +516,7 @@
 
         </section><!-- End About Section -->
 
-        <!-- ======= Values Section ======= -->
-        <section id="alur-tes" class="alur-tes">
-
-            <div class="container" data-aos="fade-up">
-
-                <header class="section-header">
-                    <h2>Alur Tes</h2>
-                    <p>Berikut Alur Tes Psikologi Online</p>
-                </header>
-
-                <div class="row">
-
-                    <div class="col-lg-4" data-aos="fade-up" data-aos-delay="200">
-                        <div class="box">
-                            <img src="{{ asset('assets/img/undraw-values-1.png') }}" class="img-fluid" alt="value-1"
-                                loading="lazy">
-                            <h3>Ad cupiditate sed est odio</h3>
-                            <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
-                        <div class="box">
-                            <img src="{{ asset('assets/img/undraw-values-2.png') }}" class="img-fluid" alt="value-2"
-                                loading="lazy">
-                            <h3>Voluptatem voluptatum alias</h3>
-                            <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="600">
-                        <div class="box">
-                            <img src="{{ asset('assets/img/undraw-values-3.png') }}" class="img-fluid" alt="value-3"
-                                loading="lazy">
-                            <h3>Fugit cupiditate alias nobis.</h3>
-                            <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.
-                            </p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+        
     </main>
     <!-- End #main -->
 
